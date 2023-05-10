d1f07a3688e3e14fbb8467d009dcc3c84111c59a
MaxBrych <max.brych03@gmail.com>
2023-05-10 08:21:26 +0200

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,43 @@
-import React from 'react';
-import { Routes, Route, Outlet, useParams, useNavigate } from 'react-router-dom';
-import { Navigation } from './components/Navigation';
-import { WalletSelectButton } from './components/WalletSelectButton';
-import { ProfileButton } from './components/ProfileButton';
-import { Posts } from './components/Posts';
-import { ProgressSpinner } from './components/ProgressSpinner';
-import { TopicSearch } from './components/TopicSearch';
-import { UserSearch } from './components/UserSearch';
-import './App.css';
+import React from "react";
+import {
+  Routes,
+  Route,
+  Outlet,
+  useParams,
+  useNavigate,
+} from "react-router-dom";
+import { Navigation } from "./components/Navigation";
+import { WalletSelectButton } from "./components/WalletSelectButton";
+import { ProfileButton } from "./components/ProfileButton";
+import { Posts } from "./components/Posts";
+import { ProgressSpinner } from "./components/ProgressSpinner";
+import { TopicSearch } from "./components/TopicSearch";
+import { UserSearch } from "./components/UserSearch";
+import "./App.css";
+import { buildQuery, arweave, createPostInfo } from "./lib/api";
 
 async function getPostInfos() {
-  return [];
+  const query = buildQuery();
+  const results = await arweave.api.post("/graphql", query).catch((err) => {
+    console.error("GraphQL query failed");
+    throw new Error(err);
+  });
+  const edges = results.data.data.transactions.edges;
+  console.log(edges);
+  return edges.map((edge) => createPostInfo(edge.node));
 }
 
 const App = () => {
-  
+  const [postInfos, setPostInfos] = React.useState([]);
+  const [isSearching, setIsSearching] = React.useState(false);
+
   React.useEffect(() => {
-    getPostInfos();
-  }, [])
+    setIsSearching(true);
+    getPostInfos().then((posts) => {
+      setPostInfos(posts);
+      setIsSearching(false);
+    });
+  }, []);
 
   return (
     <div id="app">
@@ -27,10 +47,10 @@ const App = () => {
         </aside>
         <main>
           <Routes>
-            <Route path="/" name="home" element={
-            <Home 
-
-            />}
+            <Route
+              path="/"
+              name="home"
+              element={<Home isSearching={isSearching} postInfos={postInfos} />}
             />
             <Route path="/topics" element={<Topics />}>
               <Route path="/topics/" element={<TopicSearch />} />
@@ -51,6 +71,8 @@ const Home = (props) => {
   return (
     <>
       <header>Home</header>
+      {props.isSearching && <ProgressSpinner />}
+      <Posts postInfos={props.postInfos} />
     </>
   );
 };
@@ -81,13 +103,13 @@ const TopicResults = () => {
 
   const onTopicSearch = (topic) => {
     navigate(`/topics/${topic}`);
-  }
+  };
 
   React.useEffect(() => {
     setIsSearching(true);
     setTopicPostInfos([]);
     try {
-      getPostInfos(null,topic).then(posts => { 
+      getPostInfos(null, topic).then((posts) => {
         setTopicPostInfos(posts);
         setIsSearching(false);
       });
@@ -95,15 +117,15 @@ const TopicResults = () => {
       console.logErrorg(error);
       setIsSearching(false);
     }
-  }, [topic])
+  }, [topic]);
   return (
     <>
-    <TopicSearch searchInput={topic} onSearch={onTopicSearch}/>
-    {isSearching && <ProgressSpinner />}
-    <Posts postInfos={topicPostInfos} />
+      <TopicSearch searchInput={topic} onSearch={onTopicSearch} />
+      {isSearching && <ProgressSpinner />}
+      <Posts postInfos={topicPostInfos} />
     </>
-  )
-}
+  );
+};
 
 function UserResults() {
   const [userPostInfos, setUserPostInfos] = React.useState([]);
@@ -113,27 +135,27 @@ function UserResults() {
 
   const onUserSearch = (address) => {
     navigate(`/users/${address}`);
-  }
+  };
 
   React.useEffect(() => {
     setIsSearching(true);
     try {
-      getPostInfos(addr).then(posts => { 
-        setUserPostInfos(posts); 
+      getPostInfos(addr).then((posts) => {
+        setUserPostInfos(posts);
         setIsSearching(false);
       });
     } catch (error) {
       console.logErrorg(error);
       setIsSearching(false);
     }
-  }, [addr])
+  }, [addr]);
   return (
     <>
-    <UserSearch searchInput={addr} onSearch={onUserSearch}/>
-    {isSearching && <ProgressSpinner />}
-    <Posts postInfos={userPostInfos} />
+      <UserSearch searchInput={addr} onSearch={onUserSearch} />
+      {isSearching && <ProgressSpinner />}
+      <Posts postInfos={userPostInfos} />
     </>
   );
-};
+}
 
 export default App;
